80c06cd8eb695a8e100832798ee4318c184ef1f4
Zidni Mubarock <zidmubarock@gmail.com>
2011-11-07 23:09:46 +0700

    Adding base SEF pattern

--- ci_app/config/config.php
+++ ci_app/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://'.$_SERVER['SERVER_NAME'].'/platform.rumahtiket.co
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
