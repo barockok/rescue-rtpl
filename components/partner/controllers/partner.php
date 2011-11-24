a276d86f28782824ae22dcabd4f6ff96d6888445
Zidni Mubarock <zidmubarock@gmail.com>
2011-11-24 23:38:17 +0700

    adding default controller route for partner

---
+++ components/partner/controllers/partner.php
@@ -0,0 +1,11 @@
+<?
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Partner extends Platform_Controller {
+	 
+	public function __construct()
+	 {
+	 	parent::__construct();
+	 }
+}
