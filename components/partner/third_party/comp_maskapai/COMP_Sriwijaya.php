19722b5aab8fea33851b8b22f73af1b7aef59a48
Zidni Mubarock <zidmubarock@gmail.com>
2011-11-25 19:57:41 +0700

    Fixing Sriwijaya, Curl READY...

--- components/partner/third_party/comp_maskapai/COMP_Sriwijaya.php
+++ components/partner/third_party/comp_maskapai/COMP_Sriwijaya.php
@@ -2,27 +2,34 @@
 
 class Sriwijaya extends Comp_maskapai_base{
 	
-	private $username = 'idbooking5';
-	private $password = 'indonesia';
-	private $_default_url = 'https://agent.sriwijayaair.co.id/b2b/secure/home.jsp';
-	private $_login_url = 'https://agent.sriwijayaair.co.id/b2b/secure/j_security_check';
-	private $_user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1';
+	private $username 			= 'idbooking5';
+	private $password 			= 'indonesia';
+	private $_default_url 		= 'https://agent.sriwijayaair.co.id/b2b/secure/home.jsp';
+	private $_login_url 		= 'https://agent.sriwijayaair.co.id/b2b/secure/j_security_check';
+	private $_user_agent 		= 'Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1';
+	private $_logout_url 		= 'https://agent.sriwijayaair.co.id/b2b/secure/logout.jsp';
+	private $_src_action_url 	= 'https://agent.sriwijayaair.co.id/b2b/secure/AvailabilityAction';
+	private $_src_result_url 	= 'https://agent.sriwijayaair.co.id/b2b/secure/createpnr.jsp';
+	private $_start_url 		= 'https://agent.sriwijayaair.co.id/b2b/secure/';
 	
 	function __construct() {
 		parent::__construct();
+		/*
 		foreach(parent::$_opt as $key => $val ){
 			$this->_opt->$key = $val;
 		}
+		*/
 		$this->_cookies_file = "./components/partner/third_party/comp_maskapai/cookies/sriwijaya_airline.txt";
+		$this->_ci->load->library('my_curl');
+		$this->login();
 	}
 	
 	function index() {
 		
 	}
-	
 	function mainPage(){
 		$conf = array(
-			'url'				=> $this->_login_url,
+			'url'				=> $this->_default_url,
 			'timeout'			=> 30,
 			'header'			=> 0,
 			'followlocation'	=> 1,
@@ -32,55 +39,144 @@ class Sriwijaya extends Comp_maskapai_base{
 			'ssl_verifyhost'	=> 0,
 			'useragent'			=> $this->_user_agent,
 		);	
-		$this->my_curl->setup($conf);
-		$this->my_curl->exc();
+		$this->_ci->my_curl->setup($conf);
+		return $this->_ci->my_curl->exc();
 	}
-	
-	function redirectPage(){
+	function topage($url , $return = true){
 		$conf = array(
-			'url'				=> $this->_login_url,
-			'timeout'			=> 30,
-			'header'			=> 0,
-			'followlocation'	=> 1,
-			'cookiefile'		=> $this->_cookies_file,
-			'referer'			=> $this->_default_url,
-			'returntransfer'	=> 1,
-			'ssl_verifyhost'	=> 0,
-			'useragent'			=> $this->_user_agent,
-		);	
-		$this->my_curl->setup($conf);
-		$this->my_curl->exc();
+				'url' => $url,
+				'cookiejar' 		=> $this->_cookies_file,
+				'cookiefile' 		=> $this->_cookies_file,
+				'header'		=> 0,
+				'nobody'	=> false,
+				'returntransfer' => 1,
+			//	'returntransfer' => 1
+			);
+		$this->_ci->my_curl->setup($conf);
+		$exc = $this->_ci->my_curl->exc();
+		if($return = true ) return $this->_ci->my_curl;
+		echo $exc;
+	}
+	public function start()
+	{
+		$conf = array(
+			'url' => $this->_start_url,
+			'cookiejar' 		=> $this->_cookies_file,
+			'cookiefile' 		=> $this->_cookies_file,
+			'header'		=> 0,
+			'nobody'	=> true,
+		//	'returntransfer' => 1
+		);
+		$this->_ci->my_curl->setup($conf);
+		$this->_ci->my_curl->exc();
+		
+	}
+	function login(){
+		$this->start();
+		$post_data = array(
+			'j_username' => 'idbooking5',
+			'j_password' => 'indonesia',
+			
+		);
+		$conf = array(
+			'url' 				=> $this->_login_url,
+			'timeout' 			=> 30,
+		
+			'header' 			=> 1,
+			'nobody' 			=> false,
+			'followlocation' 	=> 1,
+			'cookiejar' 		=> $this->_cookies_file,
+			'cookiefile' 		=> $this->_cookies_file,
+			'returntransfer' 	=> true,
+			'post' 				=> true,
+			'MAXREDIRS' 		=> 10,
+			'ssl_verifyhost' 	=> 0,
+			//'referer' 			=> $this->referer_url,
+			'AUTOREFERER' 		=> true,
+			'FAILONERROR' 		=> false,
+			'postfields' 		=> http_build_query( $post_data , NULL, '&' ),
+		);
+		$this->_ci->my_curl->setup($conf);
+		$this->_ci->my_curl->exc();
+		
+		
+		//https://agent.sriwijayaair.co.id/b2b/secure/logout.jsp
+	
+		
+	}
+	function somepage(){
+		$conf = array(
+				'url' => 'https://agent.sriwijayaair.co.id/b2b/secure/home.jsp',
+				'cookiejar' 		=> $this->_cookies_file,
+				'cookiefile' 		=> $this->_cookies_file,
+				'header'		=> 0,
+				'nobody'	=> false,
+			//	'returntransfer' => 1
+			);
+		$this->_ci->my_curl->setup($conf);
+		echo $this->_ci->my_curl->exc();
+	}
+	function logout(){
+		
+			$conf = array(
+					'url' => 'https://agent.sriwijayaair.co.id/b2b/secure/logout.jsp',
+					'cookiejar' 		=> $this->_cookies_file,
+					'cookiefile' 		=> $this->_cookies_file,
+					'header'		=> 0,
+					'nobody'	=> true,
+
+
+				//	'returntransfer' => 1
+				);
+			$this->_ci->my_curl->setup($conf);
+			$this->_ci->my_curl->exc();
 	}
 	
-	function login(){
-		$this->mainPage();
-		
-		$post_data = array(
-			'j_username'	=> $this->username,
-			'j_password'	=> $this->password,
+	public function _search()
+	{
+		$posted = array(
+			'isReturn' => false,
+			'from' => 'CGK',
+			'to'=>'SUB',
+			'departDate1' => 30,
+			'departDate2'=>'10-2011',
+			'adult' => 1,
+			'child' => 0,
+			'infant'=> 0,
+			'returndaterange' => 0,
+			'Submit' => 'Search',
 		);
 		
 		$conf = array(
-			'url'				=> $this->_login_url,
+			'url' 				=> $this->_src_action_url,
+			'post' 				=> true,
+			'postfields' 		=> http_build_query($posted),
 			'timeout'			=> 30,
 			'header'			=> 0,
-			'followlocation'	=> 1,
+			'followlocation'	=> true,
+			'maxredirs'			=> 10,
 			'cookiefile'		=> $this->_cookies_file,
 			'returntransfer'	=> 1,
 			'post'				=> true,
 			'referer'			=> $this->_default_url,
 			'ssl_verifyhost'	=> 0,
-			'postfields'		=> http_build_query($post_data),
 			'useragent'			=> $this->_user_agent,
 		);
 		
-		$this->my_curl->setup($conf);
-		echo $this->my_curl->exc();
+		$this->_ci->my_curl->setup($conf);
+		$this->_ci->my_curl->exc();
+		$res = $this->topage($this->_ci->my_curl->res_info('url'));
 		
-		echo $this->redirectPage();
+		return $res->res();
+	
+		
+		
+		
 	}
 	
 	function search(){
+		
+		
 		$roundTrip = false;
 		if ($roundTrip) {
 			$type = 'return';
@@ -93,7 +189,8 @@ class Sriwijaya extends Comp_maskapai_base{
 			"./components/partner/third_party/comp_maskapai/sriwijaya_html/sriwijayaSingleFlight/createpnr.jsp.html",
 		);
 		
-		$page = file_get_html($htmlFile[0]);
+		$page = str_get_html($this->_search());
+		echo $page;
 		$table = $page->find('div[id=pagewrapper] div[id=mainWrapper]',0)->find('form[action=./PNRAction]',0);
 		$flight = $page->find('table[id=table_go]',0)->find('tr table[class=flightInfo]');
 		$class = $page->find('table[id=table_go] table[class=classTable] td');
@@ -185,6 +282,7 @@ class Sriwijaya extends Comp_maskapai_base{
 	public function doSearch()
 	{
 			$this->addResult($this->cleanObject('Sriwijaya/search', array()));
+			$this->logOut();
 	}
 
 }?>
