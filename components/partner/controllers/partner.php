722102a12e691258523c8639f12c9defb433f48f
Zidni Mubarock <zidmubarock@gmail.com>
2012-01-05 08:56:23 +0700

    HMVC check git add .git add .clear

---
+++ components/partner/controllers/partner.php
@@ -0,0 +1,13 @@
+<?
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Partner extends MX_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	public function index()
+	{
+		echo 'Added Bithc';
+	}
+}
