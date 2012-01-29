247ed3dd0a7e63bd18a522402b8ce542b1f383a6
Zidni Mubarock <zidmubarock@gmail.com>
2012-01-29 17:38:28 +0700

    wrong db

--- dbscheme/rt_api.sql
+++ dbscheme/rt_api.sql
@@ -1,16 +1,199 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 3.3.9.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 29, 2012 at 05:36 PM
+-- Server version: 5.5.9
+-- PHP Version: 5.3.6
 
- Source Server         : Localhost
- Source Server Type    : MySQL
- Source Server Version : 50509
- Source Host           : localhost
- Source Database       : rt_api
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
- Target Server Type    : MySQL
- Target Server Version : 50509
- File Encoding         : utf-8
 
- Date: 01/29/2012 17:31:52 PM
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `rt_api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `apis`
+--
+
+CREATE TABLE `apis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `endpoint` varchar(150) NOT NULL,
+  `method` varchar(10) NOT NULL,
+  `description` varchar(300) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `apis`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` VALUES(1, 'Zidi', 'Mubarock', 'zidmubarock@gmail.com', 'active', '2012-01-05 20:01:56', '2012-01-05 20:02:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_apps`
+--
+
+CREATE TABLE `client_apps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `api_key` varchar(150) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `client_apps`
+--
+
+INSERT INTO `client_apps` VALUES(1, 'Base App', 1, '526342436418681', 'active', 'web', '2012-01-08 00:59:18', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `error_codes`
+--
+
+CREATE TABLE `error_codes` (
+  `code` int(11) NOT NULL,
+  `severity` varchar(30) NOT NULL,
+  `description` varchar(300) NOT NULL,
+  `var_msg` varchar(150) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `error_codes`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `error_logs`
+--
+
+CREATE TABLE `error_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(30) NOT NULL,
+  `meta` text NOT NULL,
+  `level` int(11) NOT NULL,
+  `responsible` varchar(255) NOT NULL,
+  `author_file` varchar(255) NOT NULL,
+  `backtrace` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `error_logs`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keys`
+--
+
+CREATE TABLE `keys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(40) NOT NULL,
+  `level` int(2) NOT NULL,
+  `ignore_limits` tinyint(1) NOT NULL DEFAULT '0',
+  `date_created` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `keys`
+--
+
+INSERT INTO `keys` VALUES(1, 'abc', 1, 0, 0);
+INSERT INTO `keys` VALUES(2, 'cbc', 1, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(150) NOT NULL,
+  `client_app_id` int(11) NOT NULL,
+  `end_point` varchar(150) NOT NULL,
+  `parameter` text NOT NULL,
+  `method` varchar(10) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `logs`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `token_secrets`
+--
+
+CREATE TABLE `token_secrets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `keys` varchar(150) NOT NULL,
+  `app_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `token_secrets`
+--
 
