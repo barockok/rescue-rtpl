e4daabb5af01e39c96464d77940b766808d8adb4
Zidni Mubarock <zidmubarock@gmail.com>
2012-01-29 17:32:38 +0700

    new db scheme

---
+++ dbscheme/rt_api.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50509
+ Source Host           : localhost
+ Source Database       : rt_api
+
+ Target Server Type    : MySQL
+ Target Server Version : 50509
+ File Encoding         : utf-8
+
+ Date: 01/29/2012 17:31:52 PM
+*/
+
---
+++ dbscheme/rt_pre_prod.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50509
+ Source Host           : localhost
+ Source Database       : rt_pre_prod
+
+ Target Server Type    : MySQL
+ Target Server Version : 50509
+ File Encoding         : utf-8
+
+ Date: 01/29/2012 17:31:38 PM
+*/
+
