a731e640b9911e782800ecdd689a0526bf31243a
Zidni Mubarock <zidmubarock@gmail.com>
2012-02-06 12:07:19 +0700

    again

--- application/hooks/app_hook.php
+++ application/hooks/app_hook.php
@@ -8,11 +8,11 @@ class App_hook
 	}
 	public function initial_overide_php_setting()
 	{
-/*
+
 		ini_set('display_errors','Off'); 
 		set_error_handler('_error_handler', E_ALL);
 		register_shutdown_function('_shutdown_handler');
-	*/
+	
 	}
 
 }
