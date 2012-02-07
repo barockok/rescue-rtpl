b5b69aa57974f04c7839f3f1fb38e34e6818bd01
Zidni Mubarock <zidmubarock@gmail.com>
2012-02-07 18:14:33 +0700

    test again

--- application/hooks/app_hook.php
+++ application/hooks/app_hook.php
@@ -8,7 +8,7 @@ class App_hook
 	}
 	public function initial_overide_php_setting()
 	{
-		ini_set('display_errors','On'); 
+		ini_set('display_errors','Off'); 
 	//	set_error_handler('_error_handler', E_ALL);
 	//	register_shutdown_function('_shutdown_handler');
 	}
