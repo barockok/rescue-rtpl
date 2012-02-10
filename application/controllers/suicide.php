40f78855ddc2d03186767a92ee637b9e9028c7fb
Zidni Mubarock <zidmubarock@gmail.com>
2012-02-11 01:16:05 +0700

    typo --hard

--- application/controllers/suicide.php
+++ application/controllers/suicide.php
@@ -2,16 +2,16 @@
 /**
 * 
 */
-class Suicide extends MX_Application
+class Suicide extends MX_Controller
 {
 	
-	function __construct(argument)
+	function __construct()
 	{
 		# code...
 	}
 	public function index()
 	{
-		echo 'Dont kill me, I\'ll do it by my self '
+		echo "Dont kill me, I'll do it by my self ";
 	}
 }
 
