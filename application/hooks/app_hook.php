c6976f3844569eb7a8d3dd7e22c23e521bf183f6
Zidni Mubarock <zidmubarock@gmail.com>
2012-02-28 00:05:46 +0700

    wtf with tiketcom

--- application/hooks/app_hook.php
+++ application/hooks/app_hook.php
@@ -11,6 +11,7 @@ class App_hook
 		ini_set('display_errors','On'); 
 		
 		error_reporting(E_USER_ERROR | E_RECOVERABLE_ERROR | E_ERROR);
+
 		register_shutdown_function('_shutdown_handler');
 		set_error_handler('_error_handler');
 		set_exception_handler('exception_handler');
@@ -26,7 +27,13 @@ class App_hook
 
 }
 function exception_handler($exception) {
-  echo "Uncaught exception: " , $exception->getMessage(), "\n";
+  	
+		$output = json_encode(array('error' => 'Something really bad happens, we so sorry about this, we gonna fix it immediately'));
+		
+		header('HTTP/1.1: ' . 500);
+		header('Status: ' . 500);
+		header('Content-Length: ' . strlen($output));
+		exit ($output);
 }
 
 
@@ -51,22 +58,22 @@ function _error_handler($errno, $errstr, $errfile, $errline)
 			case E_USER_NOTICE:
 			break;
 		    case E_USER_ERROR:
-				$return['error_constant'] = 'E_USER_ERROR';
+				$return['ERROR_CONSTANT'] = 'E_USER_ERROR';
 		     	_output_error(array('error' => 'unknow'), 500, $return , TRUE, TRUE, TRUE);
 		        break;
 
 		    case E_USER_WARNING:
-				$return['error_constant'] = 'E_USER_ERROR';
+				$return['ERROR_CONSTANT'] = 'E_USER_ERROR';
 	      		_output_error(array('error' => 'unknow'), 500, $return , FALSE);
 		        break;
 
 		    case E_USER_NOTICE:
-				$return['error_constant'] = 'E_USER_ERROR';
+				$return['ERROR_CONSTANT'] = 'E_USER_ERROR';
 		      	_output_error(array('error' => 'unknow'),  500, $return, FALSE );
 		        break;
 
 		    default:
-				$return['error_constant'] = 'E_UNKNOW';
+				$return['ERROR_CONSTANT'] = 'E_UNKNOW';
 		     	_output_error(array('error' => 'unknow'),  500, $return , TRUE, TRUE, TRUE);
 		        break;
 		    }
@@ -78,12 +85,9 @@ function _output_error($data = array(), $http_code = 500, $error_data = FALSE, $
 {	
 	
 		if($db == TRUE):
-
 			
 		endif;
-		
-		
-		
+	
 		// sending email
 	
 		$output = json_encode($data);
@@ -91,7 +95,7 @@ function _output_error($data = array(), $http_code = 500, $error_data = FALSE, $
 		header('Status: ' . $http_code);
 		header('Content-Length: ' . strlen($output));
 		if($exit == TRUE) exit($output);
-		echo $output;
+		echo print_r($data);
 	
 }
 
@@ -100,8 +104,8 @@ function _shutdown_handler()
 	
 	$error = error_get_last();
 	if($error['type'] == E_ERROR){
-		trigger_error($error['message'], E_USER_ERROR);
+	//	exit(json_encode($error));
+		_error_handler(E_USER_ERROR, $error['message'], $error['file'], $error['line']);
 	}
-
 	
 }
