cce389bffbff85f662abdd81c1cd49792acc96e5
Zidni Mubarock <zidmubarock@gmail.com>
2012-02-28 19:21:27 +0700

    investigate content length issue

--- application/core/REST_Controller.php
+++ application/core/REST_Controller.php
@@ -302,8 +302,9 @@ class REST_Controller extends CI_Controller {
 		header('Status: ' . $http_code);
 	//	if(WORKING_STATUS != true)
 	//	header('Content-Length: '.strlen($output));
-	
-		exit($output);
+		$this->output->set_output($output);
+		echo $this->output->get_output();
+		//exit($output);
 	}
 
 	/*
