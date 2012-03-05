d73bf3385d32627992753e735ce978818349626d
Zidni Mubarock <zidmubarock@gmail.com>
2012-03-05 16:10:07 +0700

    Extra exception for Ailines when service down and login fail

--- components/service/third_party/comp_maskapai/AirlinesExceptions.php
+++ components/service/third_party/comp_maskapai/AirlinesExceptions.php
@@ -3,6 +3,18 @@
 * 
 */
 class AirlinesExceptions extends Exception {}
+class ServiceDown extends AirlinesExceptions{
+	public function __construct($company)
+	{
+		$this->message = "$company service currently down";
+	}
+}
+class LoginFailed extends AirlinesExceptions{
+	public function __construct($company)
+	{
+		$this->message = "$company Login Fail";
+	}
+}
 class ResultFareNotFound extends AirlinesExceptions{
 	public function __construct($log)
 	{
